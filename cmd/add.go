4892f4b492118f2a5b6f6c2b9f4760da3a3e8846
gosselinchristian <gosselinchristian@gmail.com>
2020-06-03 21:07:46 -0400

    add cmd

--- cmd/add.go
+++ cmd/add.go
@@ -6,4 +6,11 @@ var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Add a new Todo.",
 	Long:  "This commands adds a new Todo to the list stored on the local file system.",
+	Run: func(cmd *cobra.Command, args []string) {
+		// yata := main.NewYata()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
 }
